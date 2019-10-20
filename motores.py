d4ab5c966b8d53d290b2c78523f0e3c3ef66a670
vitormaco <vitorcorrea1050@hotmail.com>
2019-10-20 17:25:13 -0300

    Adicionado controle dos motores

---
+++ motores.py
@@ -0,0 +1,81 @@
+"""DC motor test."""
+import RPi.GPIO as GPIO
+
+in1 = 14
+in2 = 15
+servopin = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(servopin, GPIO.OUT)
+outA = GPIO.PWM(in1, 1000)
+outB = GPIO.PWM(in2, 1000)
+servo = GPIO.PWM(servopin, 50)
+outA.start(0)
+outB.start(0)
+servo.start(7.5)
+
+print("\n")
+print("w-frente s-tras x-para q-sai")
+print("a-esquerda d-direita c-reto 1,2,3 - velocidades")
+print("\n")
+
+speed = 100
+
+while True:
+
+    x = input()
+    if x == 'x':
+        print("parei")
+        outA.ChangeDutyCycle(0)
+        outB.ChangeDutyCycle(0)
+        x = 'z'
+
+    elif x == 'w':
+        print("andando")
+        outA.ChangeDutyCycle(speed)
+        outB.ChangeDutyCycle(0)
+        x = 'z'
+
+    elif x == 's':
+        print("voltando")
+        outA.ChangeDutyCycle(0)
+        outB.ChangeDutyCycle(speed)
+        x = 'z'
+
+    elif x == 'a':
+        print("virando pra esquerda")
+        servo.ChangeDutyCycle(6)
+        x = 'z'
+
+    elif x == 'd':
+        print("virando pra direita")
+        servo.ChangeDutyCycle(9)
+        x = 'z'
+
+    elif x == 'c':
+        print("reto")
+        servo.ChangeDutyCycle(7.5)
+        x = 'z'
+
+    elif x == '1':
+        print("marcha 1")
+        speed = 50
+        x = 'z'
+
+    elif x == '2':
+        print("marcha 2")
+        speed = 75
+        x = 'z'
+
+    elif x == '3':
+        print("marcha 3")
+        speed = 100
+        x = 'z'
+
+    elif x == 'q':
+        GPIO.cleanup()
+        print("flws")
+        break
+
