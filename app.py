0a5a7c5fffb51335f7d2e4823d2cb6063987ec59
vitormaco <vitorcorrea1050@hotmail.com>
2019-10-26 12:04:41 -0300

    Remove comments

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 from importlib import import_module
 import os
 from flask import Flask, render_template, Response
-
-# Raspberry Pi camera module (requires picamera package)
 from camera_pi import Camera
 from dht import readDHTSensor
 from gas import readGasSensor
@@ -12,12 +10,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Video streaming home page."""
     return render_template('index.html')
 
 
 def videogen(camera):
-    """Video streaming generator function."""
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
@@ -26,20 +22,17 @@ def videogen(camera):
 
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(videogen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/dht')
 def dht():
-    """Humidity and Temperature sensor."""
     return Response(readDHTSensor(), mimetype='text/xml')
 
 
 @app.route('/gas')
 def gas():
-    """Humidity and Temperature sensor."""
     return Response(readGasSensor(), mimetype='text/xml')
 
 
